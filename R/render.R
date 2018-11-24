c5d8c05db29f636d3094631f4c15e3b9d1cefba6
Florian Privé <florian.prive.21@gmail.com>
2018-11-24 07:18:53 +0100

    solution that works

--- R/render.R
+++ R/render.R
@@ -9,3 +9,16 @@ my_render <- function(rmd) {
     encoding = "UTF-8"
   )
 }
+
+#' Render an Rmd file
+#' @param rmd Path of the R Markdown file to render.
+#' @export
+my_render2 <- function(rmd) {
+  cl <- parallel::makePSOCKcluster(1)
+  on.exit(parallel::stopCluster(cl), add = TRUE)
+  parallel::clusterExport(cl, "rmd", envir = environment())
+  parallel::clusterEvalQ(cl, {
+    rmarkdown::render(rmd, "all", encoding = "UTF-8")
+  })[[1]]
+}
+
